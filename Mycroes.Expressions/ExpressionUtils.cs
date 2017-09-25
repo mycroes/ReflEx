ebae5536818ad209a3f1055bae27feaad8c53c33
Michael Croes <mycroes@gmail.com>
2017-09-25 23:33:56 +0200

    Add Mycroes.Expressions project
    Contains:
    - ExpressionUtils.EvaluateExpression
    - Extensions.Evaluate

---
+++ Mycroes.Expressions/ExpressionUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Mycroes.Expressions
+{
+    public static class ExpressionUtils
+    {
+        public static T EvaluateExpression<T>(Expression expression)
+        {
+            if (!typeof(T).IsAssignableFrom(expression.Type))
+                throw new ArgumentException(
+                    $"Expression return type {expression.Type} is not assignable to {typeof(T)}.");
+
+            if (Nullable.GetUnderlyingType(typeof(T)) == expression.Type)
+            {
+                var ctor = typeof(T).GetConstructor(new[] {expression.Type}) ??
+                    throw new Exception(
+                        $"Missing single argument constructor with parameter of type {expression.Type} on type {typeof(T)}");
+                expression = Expression.New(ctor, expression);
+            }
+            else if (typeof(T) == typeof(object) && expression.Type.IsValueType)
+            {
+                expression = Expression.Convert(expression, typeof(T));
+            }
+
+            var lambda = Expression.Lambda<Func<T>>(expression);
+            return lambda.Compile().Invoke();
+        }
+    }
+}
---
+++ Mycroes.Expressions/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace Mycroes.Expressions
+{
+    public static class Extensions
+    {
+        public static T Evaluate<T>(this Expression expression) => ExpressionUtils.EvaluateExpression<T>(expression);
+    }
+}
